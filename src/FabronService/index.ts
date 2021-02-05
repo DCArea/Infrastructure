f9adcd1c8311f5d7b3c61310c722742fd7e7b56f
Sun Zhongfeng <csyszf@outlook.com>
2021-02-05 18:11:57 +0800

    namespace & pull image secret

--- src/FabronService/index.ts
+++ src/FabronService/index.ts
@@ -1,21 +1,6 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as kx from "@pulumi/kubernetesx";
 
+// import { namespace_fabron, secret_dca_regcred } from "./core"
 
-const appLabels = { app: "nginx" };
-const namespace_foo = new k8s.core.v1.Namespace("foo");
-
-const deployment = new k8s.apps.v1.Deployment("nginx", {
-    metadata: {
-        namespace: namespace_foo.metadata.name
-    },
-    spec: {
-        selector: { matchLabels: appLabels },
-        replicas: 1,
-        template: {
-            metadata: { labels: appLabels },
-            spec: { containers: [{ name: "nginx", image: "nginx" }] }
-        }
-    }
-});
-export const name = deployment.metadata.name;
+export * from "./core"
