01f4b103f7b0d1622906475e96ec142bb86f1821
Sun Zhongfeng <csyszf@outlook.com>
2021-02-26 18:15:35 +0800

    setup letsencrypt cluster issuer

--- src/cert-manager/index.ts
+++ src/cert-manager/index.ts
@@ -1,4 +1,5 @@
 import * as k8s from "@pulumi/kubernetes";
+import * as crds from "./crds"
 
 function create_cert_manager() {
     const name_default = "cert-manager";
@@ -13,4 +14,27 @@ function create_cert_manager() {
     });
 }
 
-create_cert_manager()
+function create_cluster_issuer() {
+    var ci = new crds.certmanager.v1.ClusterIssuer("letsencrypt", {
+        metadata: { name: "letsencrypt" },
+        spec: {
+            acme: {
+                server: "https://acme-v02.api.letsencrypt.org/directory",
+                email: "suraciii@outlook.com",
+                privateKeySecretRef: { name: "letsencrypt" },
+                solvers: [
+                    {
+                        http01: {
+                            ingress: {
+                                class: "nginx"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    })
+}
+
+create_cert_manager();
+create_cluster_issuer();
