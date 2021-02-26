15286d6cd663b9baac03d58a06608d935f448324
Sun Zhongfeng <csyszf@outlook.com>
2021-02-26 10:30:28 +0800

    move cert-manager to a new project

---
+++ src/cert-manager/index.ts
@@ -0,0 +1,16 @@
+import * as k8s from "@pulumi/kubernetes";
+
+function create_cert_manager() {
+    const name_default = "cert-manager";
+    const namespace = new k8s.core.v1.Namespace(name_default, { metadata: { name: name_default } })
+    const cert_manager = new k8s.helm.v3.Chart(name_default, {
+        repo: "jetstack",
+        chart: name_default,
+        namespace: name_default,
+        values: {
+            installCRDs: true
+        }
+    });
+}
+
+create_cert_manager()
